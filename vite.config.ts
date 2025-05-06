b2834142e462f09756cc52d5508ff508e2ec42d8
chaima3007 <chaimagheriballah@gmail.com>
2025-05-06 22:42:57 +0200

    correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	
-});
+  plugins: [react(), tsconfigPaths()],
+})
+
