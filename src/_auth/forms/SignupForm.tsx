3366a16bc42944e45794dbac2bbf9414ed9a2508
Chaima GHERIBALLAH <158222954+chaima3007@users.noreply.github.com>
2025-05-09 13:48:36 +0200

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -90,7 +90,7 @@ const SignupForm = () => {
 					Create a new account
 				</h2>
 				<p className="text-light-3 small-medium md:base-regular mt-2">
-					To use Snapgram, please enter your account details
+					To use HalaShare, please enter your account details
 				</p>
 
 				<form
